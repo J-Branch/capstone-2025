b379906fced0e64f2b4b757d406215915754d80d
EricGuti11 <xcobravisionx@gmail.com>
2025-04-03 16:09:54 -0400

    finished my characters hitboxes for attacks

--- characters/SwordFighter/SwordFighter.gd
+++ characters/SwordFighter/SwordFighter.gd
@@ -46,7 +46,7 @@ func create_hitbox(width, height, damage, angle, base_kb, kb_scaling, duration, 
 	# Rotates the points 
 	if dir: # looking left
 		var flip_x_points = Vector2(-points.x, points.y)
-		hitbox_instance.set_parameters(width, height, damage, -angle+180, base_kb, kb_scaling, duration, type, points, angle_flipper, hitlag)
+		hitbox_instance.set_parameters(width, height, damage, -angle+180, base_kb, kb_scaling, duration, type, flip_x_points, angle_flipper, hitlag)
 	else: # looking right
 		hitbox_instance.set_parameters(width, height, damage, angle, base_kb, kb_scaling, duration, type, points, angle_flipper, hitlag)
 	return hitbox_instance
@@ -83,44 +83,88 @@ func B_DOWN():
 		return true
 		
 func B_SIDE():
-	if frame > 40:
+	if frame == 16:
+		create_hitbox(35.5,13,8, 0, 0, 0, 15, 'normal', Vector2(33.2,-2.5), 0, 1)
+	if frame >= 32:
 		return true
 
 func B_NEUTRAL():
-	if frame > 40:
+	if frame == 16:
+		create_hitbox(27,31,8, 0, 0, 0, 8, 'normal', Vector2(26.5,-8.5), 0, 1)
+	if frame >= 24:
 		return true
 
 # Air Attacks
 func A_DOWN():
-	pass
+	if frame == 21:
+		create_hitbox(11,17.5,8, 0, 0, 0, 8, 'normal', Vector2(0.5,21.2), 0, 1)
+		create_hitbox(57,21.7,8, 0, 0, 0, 8, 'normal', Vector2(-4.5,38.1), 0, 1)
+	if frame == 30:
+		return true
 
 func A_SIDE():
-	pass
+	if frame == 10:
+		create_hitbox(32.75,25.4,8, 0, 0, 0, 4, 'normal', Vector2(8.62,-26.28), 0, 1)
+		create_hitbox(20,31,8, 0, 0, 0, 4, 'normal', Vector2(34,-7.5), 0, 1)
+	if frame == 14:
+		create_hitbox(30,17,8, 0, 0, 0, 9, 'normal', Vector2(34,-0.5), 0, 1)
+	if frame >= 24:
+		return true
 
 func A_NEUTRAL():
-	pass
+	if frame == 14:
+		create_hitbox(59,24.5,8, 0, 0, 0, 7, 'normal', Vector2(3.5,-7.25), 0, 1)
+	if frame >= 22:
+		return true
 	
 # TRANSFORMED ATTACKS
 
 # Ground Attacks
 func T_B_DOWN():
-	if frame > 30:
+	if frame == 20:
+		create_hitbox(46.375,62,8, 0, 0, 0, 5, 'normal', Vector2(22.813,-18), 0, 1)
+	if frame == 25:
+		create_hitbox(46.375,25,8, 0, 0, 0, 2, 'normal', Vector2(31,8.5), 0, 1)
+	if frame >= 28:
 		return true
 	
 func T_B_SIDE():
-	if frame > 30:
+	if frame == 10:
+		create_hitbox(31.5,44.25,8, 0, 0, 0, 5, 'normal', Vector2(26,-4), 0, 1)
+	if frame == 15:
+		create_hitbox(34.75,61.125,8, 0, 0, 0, 4, 'normal', Vector2(32.625,-15.437), 0, 1)
+	if frame == 19:
+		create_hitbox(34.75,40,8, 0, 0, 0, 6, 'normal', Vector2(23,-14), 0, 1)
+	if frame >= 26:
 		return true
 
 func T_B_NEUTRAL():
-	if frame > 30:
+	if frame == 19:
+		create_hitbox(31.5,44.25,8, 0, 0, 0, 9, 'normal', Vector2(34.25,-3.125), 0, 1)
+	if frame >= 30:
 		return true
 
 # Air Attacks
 func T_A_DOWN():
-	pass
+	if frame == 10:
+		create_hitbox(50,25,8, 0, 0, 0, 6, 'normal', Vector2(2,13), 0, 1)
+	if frame == 16:
+		create_hitbox(73,42.25,8, 0, 0, 0, 6, 'normal', Vector2(3,32.125), 0, 1)
+	if frame >= 22:
+		return true
 
 func T_A_SIDE():
-	pass
+	if frame == 12:
+		create_hitbox(63.5,50.125,8, 0, 0, 0, 3, 'normal', Vector2(17.25,-18.062), 0, 1)
+	if frame == 15:
+		create_hitbox(33,34,8, 0, 0, 0, 9, 'normal', Vector2(35,-6), 0, 1)
+	if frame >= 24:
+		return true
 
 func T_A_NEUTRAL():
-	pass
+	if frame == 11:
+		create_hitbox(58,54,8, 0, 0, 0, 6, 'normal', Vector2(3,-21), 0, 1)
+	if frame == 17:
+		create_hitbox(59,35,8, 0, 0, 0, 6, 'normal', Vector2(3.5,-9), 0, 1)
+	if frame >= 24:
+		return true
