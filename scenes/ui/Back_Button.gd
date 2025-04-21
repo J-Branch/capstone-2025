7b638618ea1ab6efb27b31d088c27ae454fe71ae
EricGuti11 <xcobravisionx@gmail.com>
2025-04-20 23:05:21 -0400

    fixed many bugs and import problems

--- scenes/ui/Back_Button.gd
+++ scenes/ui/Back_Button.gd
@@ -1,16 +1,7 @@
 extends TextureButton
 
-func _on_top_area_entered(area):
-	area.get_parent().global_position = Vector2(area.get_parent().global_position.x, 650)
+@export var path = ''
 
-
-func _on_bottom_area_entered(area):
-	area.get_parent().global_position = Vector2(area.get_parent().global_position.x, 50)
-
-
-func _on_right_area_entered(area):
-	area.get_parent().global_position = Vector2(100, area.get_parent().global_position.y)
-
-
-func _on_left_area_entered(area):
-	area.get_parent().global_position = Vector2(1200, area.get_parent().global_position.y)
+func _on_pressed():
+	if(path != ''):
+		get_tree().change_scene_to_file(path)
--- scenes/ui/CSS/Boundaries.gd
+++ scenes/ui/CSS/Boundaries.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_top_area_entered(area):
+	area.get_parent().global_position = Vector2(area.get_parent().global_position.x, 650)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_bottom_area_entered(area):
+	area.get_parent().global_position = Vector2(area.get_parent().global_position.x, 50)
+
+
+func _on_right_area_entered(area):
+	area.get_parent().global_position = Vector2(100, area.get_parent().global_position.y)
+
+
+func _on_left_area_entered(area):
+	area.get_parent().global_position = Vector2(1200, area.get_parent().global_position.y)
